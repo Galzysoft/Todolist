311dde0b1db6b5032a786d55fb526d94e72ec957
XtarNodde <onyekaejimns@gmail.com>
2022-08-09 17:00:49 +0100

    ui updates

---
+++ lib/screens/todo/pagewidget/todoCard.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class TodoCard extends StatelessWidget {
+  TodoCard({
+    Key? key,
+  }) : super(key: key);
+  GlobalKey mykey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+          height: 100,
+          width: MediaQuery.of(context).size.width,
+          child: Material(
+            elevation: 2,
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+            shadowColor: Colors.purple,
+            child: Dismissible(
+              key: mykey,
+              direction: DismissDirection.horizontal,
+              onDismissed: (dismissdirection) {},
+              background: Container(color: Colors.purple,
+                child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.delete,
+                      color: Colors.white,size: 50,
+                    ),
+                  ],
+                ),
+              ),
+              child: SizedBox(
+                height: 100,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.notes,
+                    color: Colors.purple,
+                  ),
+                  title: Text(
+                    "Title",
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.w400),
+                  ),
+                  subtitle: Row(children: [
+                    Icon(
+                      Icons.date_range_outlined,
+                      color: Colors.purple,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("12-13-2022",
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.w400))
+                  ]),
+                  trailing: SizedBox(width: 200,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                         crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          SizedBox(height: 25,),
+                          FittedBox(
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                              Icon(
+                                Icons.alarm_add_sharp,
+                                color: Colors.purple,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text("12:13 pm",
+                                  style: TextStyle(
+                                      fontSize: 18, fontWeight: FontWeight.w400))
+                            ]),
+                          )
+                        ]),
+                  ),onTap:(){} ,
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
--- lib/screens/todo/todo.dart
+++ lib/screens/todo/todo.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoapp/screens/todo/controllers/todoController.dart';
+import 'package:todoapp/screens/todo/pagewidget/todoCard.dart';
 
 class Todo extends StatefulWidget {
   const Todo({Key? key}) : super(key: key);
@@ -29,50 +30,55 @@ class _TodoState extends State<Todo> {
           color: Color(0xffF0F0F0),
           elevation: 2,
           shadowColor: Colors.purple,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Spacer(flex: 2),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  // Spacer(flex: 7,),
-                  Row(
-                    children: [
-                      Text("Todo",
-                          style: TextStyle(color: Colors.purple, fontSize: 40)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 13.0, left: 10),
-                        child: SizedBox(
-                            height: 80,
-                            width: 300,
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  prefixIcon: Icon(Icons.search)),
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(begin: Alignment.topLeft,end: Alignment.bottomCenter,
+                    colors: [Colors.purple.withOpacity(0.3), Colors.white70,Colors.white])),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Spacer(flex: 2),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    // Spacer(flex: 7,),
+                    Row(
+                      children: [
+                        Text("Todo",
+                            style:
+                                TextStyle(color: Colors.purple, fontSize: 40)),
+                        Padding(
+                            padding: const EdgeInsets.only(
+                                top: 13.0, left: 10, right: 10),
+                            child: SizedBox(
+                              height: 80,
+                              width: 300,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.search),
+                                  border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20)),
+                                    borderSide: BorderSide(
+                                        style: BorderStyle.solid,
+                                        width: 2,
+                                        color: Colors.purple),
+                                  ),
+                                ),
+                              ),
                             )),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
       body: ListView.separated(
           itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                  height: 100,
-                  width: 400,
-                  child: Material(
-                    elevation: 2,
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    shadowColor: Colors.purple,
-                  )),
-            );
+            return TodoCard();
           },
           separatorBuilder: (context, index) {
             return SizedBox(
